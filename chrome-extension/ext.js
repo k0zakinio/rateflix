de0809e0f5c3e62b6e11fa005b1cdbf3635a662f
k0zakinio <adowellphoto@gmail.com>
2017-06-24 19:33:01 +0100

    now modifies the DOM to include an IMDB rating when hovering over a movie title from the netflix.com/browse end point

--- chrome-extension/ext.js
+++ chrome-extension/ext.js
@@ -1,26 +1,33 @@
-var cards = document.querySelectorAll(".title_card");
 var titleCards = [];
-cards.forEach(c => {
-  var t = c.getAttribute("aria-label");
-  if(t != null) titleCards.push(c);
-});
 
-titleCards.forEach(elem => {
-  addDelayListener(elem); 
-});
+setInterval(() => {
+  var cards = document.querySelectorAll(".title_card");
+  cards.forEach(c => {
+    var t = c.getAttribute("aria-label");
+    if(t != null) titleCards.push(c);
+  });
+  titleCards.forEach(elem => addDelayListener(elem)); 
+}, 1000);
 
 function addDelayListener(elem) {
+  if(elem.getAttribute('has-listener') !== null) return;
   var timeout = null;
-  elem.addEventListener('mouseenter', (e) => timeout = setTimeout(() => omdbRequest(e.toElement.getAttribute("aria-label"), successCallback, failureCallback), 1000));
+  function setListener(e) {
+    timeout = setTimeout(() => omdbRequest(e.toElement, successCallback, failureCallback), 1000);
+  };
+
+  elem.addEventListener('mouseenter', setListener);
   elem.addEventListener('mouseleave', () => clearTimeout(timeout));
+  elem.setAttribute('has-listener', true);
 }
 
-function omdbRequest(title, success, failure) {
+function omdbRequest(elem, success, failure) {
+  var title = elem.getAttribute("aria-label");
   var url = "https://lit-headland-12280.herokuapp.com/title/" + encodeURIComponent(title);
   var xml = new XMLHttpRequest();
   xml.onreadystatechange = function () {
     if (xml.readyState === 4 && xml.status == 200) {
-      success(JSON.parse(xml.responseText));
+      success(JSON.parse(xml.responseText), elem);
     } else if (xml.readyState === 4 && xml.status != 200) {
       failure(xml.status);
     }
@@ -30,8 +37,8 @@ function omdbRequest(title, success, failure) {
 }
 
 
-function successCallback(json) {
-  alert("imdb rating of " + json.Title + " is: " + json.imdbRating);
+function successCallback(json, elem) {
+  elem.querySelector('.meta').innerHTML += "<span class='duration'>IMDB:" + json.imdbRating + "</span>";
 };
 
 function failureCallback(statusCode) {
