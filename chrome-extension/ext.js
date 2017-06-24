d4e3902d290f1123da16ba784ec413df4cb2b58b
k0zakinio <adowellphoto@gmail.com>
2017-06-24 17:21:10 +0100

    created chrome extension

---
+++ chrome-extension/ext.js
@@ -0,0 +1,43 @@
+var cards = document.querySelectorAll(".title_card");
+var titleCards = [];
+cards.forEach(c => {
+  var t = c.getAttribute("aria-label");
+  if(t != null) titleCards.push(c);
+});
+
+titleCards.forEach(elem => {
+  addDelayListener(elem); 
+});
+
+
+function addDelayListener(elem) {
+  var timeout = null;
+  elem.addEventListener('mouseenter', (e) => timeout = setTimeout(() => omdbRequest(e.toElement.getAttribute("aria-label"), successCallback, failureCallback), 1000));
+  elem.addEventListener('mouseleave', () => clearTimeout(timeout));
+}
+
+function listenerCallback(e) {
+  }
+
+function omdbRequest(title, success, failure) {
+  var url = "http://localhost:8000/title/" + encodeURIComponent(title);
+  var xml = new XMLHttpRequest();
+  xml.onreadystatechange = function () {
+    if (xml.readyState === 4 && xml.status == 200) {
+      success(JSON.parse(xml.responseText));
+    } else if (xml.readyState === 4 && xml.status != 200) {
+      failure(xml.status);
+    }
+  };
+  xml.open("GET", url, true);
+  xml.send();
+}
+
+
+function successCallback(json) {
+  alert(json);
+};
+
+function failureCallback(statusCode) {
+  console.log("something went wrong... status code " + statusCode + " from omdb request");
+};
