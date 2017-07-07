39a8459d3a0b626f924d32d76b5edf76735b12a8
Andrew Dowell <adowellphoto@gmail.com>
2017-07-07 12:16:53 +0100

    use http as image scheme not https

--- app/app.js
+++ app/app.js
@@ -40,9 +40,9 @@ function addImgLinks(json) {
   if (json["Ratings"] !== null) {
     json["Ratings"].forEach(sourceAndValue => {
       switch(sourceAndValue["Source"]) {
-        case "Internet Movie Database": sourceAndValue["img"] = "https://www.andydowell.co.uk/images/imdb.png"; break;
-        case "Rotten Tomatoes": sourceAndValue["img"] = "https://www.andydowell.co.uk/images/rottentomatoes.png"; break;
-        case "Metacritic": sourceAndValue["img"] = "https://www.andydowell.co.uk/images/metacritic.png"; break;
+        case "Internet Movie Database": sourceAndValue["img"] = "http://www.andydowell.co.uk/images/imdb.png"; break;
+        case "Rotten Tomatoes": sourceAndValue["img"] = "http://www.andydowell.co.uk/images/rottentomatoes.png"; break;
+        case "Metacritic": sourceAndValue["img"] = "http://www.andydowell.co.uk/images/metacritic.png"; break;
       }
     });
   }
--- chrome-extension/ext.js
+++ chrome-extension/ext.js
@@ -1,52 +1,56 @@
 setInterval(() => {
-    const titleCards = [];
-    const cards = document.querySelectorAll(".title_card");
-    cards.forEach(c => {
-        const t = c.getAttribute("aria-label");
-        if (t !== null) titleCards.push(c);
-    });
-    titleCards.forEach(elem => addDelayListener(elem));
+  const titleCards = [];
+  const cards = document.querySelectorAll(".title_card");
+  cards.forEach(c => {
+    const t = c.getAttribute("aria-label");
+    if (t !== null) titleCards.push(c);
+  });
+  titleCards.forEach(elem => addDelayListener(elem));
 }, 1000);
 
 function addDelayListener(elem) {
-    if (elem.getAttribute('has-listener') !== null) {
-        return;
-    }
-    let timeout = null;
+  if (elem.getAttribute('has-listener') !== null) {
+    return;
+  }
+  let timeout = null;
 
-    function setListener(e) {
-        timeout = setTimeout(() => omdbRequest(e.toElement, successCallback, failureCallback), 500);
-    }
+  function setListener(e) {
+    timeout = setTimeout(() => omdbRequest(e.toElement, successCallback, failureCallback), 500);
+  }
 
-    elem.addEventListener('mouseenter', setListener);
-    elem.addEventListener('mouseleave', () => clearTimeout(timeout));
-    elem.setAttribute('has-listener', true);
+  elem.addEventListener('mouseenter', setListener);
+  elem.addEventListener('mouseleave', () => clearTimeout(timeout));
+  elem.setAttribute('has-listener', true);
 }
 
 function omdbRequest(elem, success, failure) {
-    const title = elem.getAttribute("aria-label");
-    const url = "https://lit-headland-12280.herokuapp.com/title/" + encodeURIComponent(title);
-    const xml = new XMLHttpRequest();
-    xml.onreadystatechange = () => {
-        if (xml.readyState === 4 && xml.status === 200) {
-            success(JSON.parse(xml.responseText), elem);
-        } else if (xml.readyState === 4 && xml.status !== 200) {
-            failure(xml.status);
-        }
-    };
-    xml.open("GET", url, true);
-    xml.send();
+  const title = elem.getAttribute("aria-label");
+  const url = "https://lit-headland-12280.herokuapp.com/title/" + encodeURIComponent(title);
+  const xml = new XMLHttpRequest();
+  xml.onreadystatechange = () => {
+    if (xml.readyState === 4 && xml.status === 200) {
+      success(JSON.parse(xml.responseText), elem);
+    } else if (xml.readyState === 4 && xml.status !== 200) {
+      failure(xml.status);
+    }
+  };
+  xml.open("GET", url, true);
+  xml.send();
 }
 
 
 function successCallback(json, elem) {
-    const meta = elem.querySelector('.meta');
-    if (meta !== null && meta.querySelector('.duration[data="rating"]') === null) {
-        meta.innerHTML += "<span class='duration' data='rating'>IMDB:" + json.imdbRating + "</span>";
-    } else {
-        console.log("ERROR: not adding html as there should already be html");
-    }
+  const target = elem.querySelector('.bob-overlay');
+  if (target !== null && target.querySelector('.rateflix[data="rating"]') === null) {
+    json["Ratings"].forEach(rating => {
+      let image = "<img src='"+rating["img"]+"' height='24px' width='24px' alt='"+rating["Source"]+"'>";
+      target.innerHTML +=  image + "<span class='rateflix' data='rating'> : " + rating["Value"] + "</span></br>";
+    });
+  } else {
+    console.log("ERROR: not adding html as there should already be html");
+  }
 }
+
 function failureCallback(statusCode) {
-    console.log("something went wrong... status code " + statusCode + " from omdb request");
+  console.log("something went wrong... status code " + statusCode + " from omdb request");
 }
